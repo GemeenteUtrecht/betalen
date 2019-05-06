addfd6413b8f2a7177ffb3a24e1d6fcb13fa0c44
Matthias Oliveiro <matthiasoliveiro@gmail.com>
2019-05-06 11:29:00 +0200

    typo's

--- src/Entity/Factuur.php
+++ src/Entity/Factuur.php
@@ -187,7 +187,7 @@ class Factuur implements StringableInterface
 	public $bronOrganisatie;	
 	
 	/**
-	 * Het tijdstip waarop dit Ambtenaren object is aangemaakt
+	 * Het tijdstip waarop dit Factuur object is aangemaakt
 	 *
 	 * @var string Een "Y-m-d H:i:s" waarde bijvoorbeeld "2018-12-31 13:33:05" ofwel "Jaar-dag-maand uur:minuut:seconde"
 	 * @Gedmo\Timestampable(on="create")
@@ -200,7 +200,7 @@ class Factuur implements StringableInterface
 	public $registratiedatum;
 	
 	/**
-	 * Het tijdstip waarop dit Ambtenaren object voor het laatst is gewijzigd.
+	 * Het tijdstip waarop dit Factuur object voor het laatst is gewijzigd.
 	 *
 	 * @var string Een "Y-m-d H:i:s" waarde bijvoorbeeld "2018-12-31 13:33:05" ofwel "Jaar-dag-maand uur:minuut:seconde"
 	 * @Gedmo\Timestampable(on="update")
